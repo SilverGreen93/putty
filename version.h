d5d871ab1b262c52a6e84935ca0b9010da1e5f23
SilverGreen93 <mihai.vasiliu.93@gmail.com>
2025-03-18 10:58:22 +0200

    Add Release information

--- version.h
+++ version.h
@@ -8,6 +8,6 @@
  * default stuff used for local development runs of 'make'.
  */
 
-#define TEXTVER "Unidentified build"
+#define TEXTVER "Release 0.83 Plus"
 #define SSHVER "-Unidentified-Local-Build"
-#define BINARY_VERSION 0,0,0,0
+#define BINARY_VERSION 0,83,0,0
