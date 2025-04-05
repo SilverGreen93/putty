a72ac5265702466cc8e02262bc13a6dd6a0d563b
SilverGreen93 <mihai.vasiliu.93@gmail.com>
2025-04-05 13:04:59 +0300

    Prepare R2 release

--- version.h
+++ version.h
@@ -8,6 +8,6 @@
  * default stuff used for local development runs of 'make'.
  */
 
-#define TEXTVER "Release 0.83 Plus R1"
+#define TEXTVER "Release 0.83 Plus R2"
 #define SSHVER "-Unidentified-Local-Build"
-#define BINARY_VERSION 0,83,0,0
+#define BINARY_VERSION 0,83,0,2
